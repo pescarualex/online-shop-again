890b1cedf32cf9a68266644681e3162b6127e28b
pescarualex <pescarualexflorin@gmail.com>
2022-07-09 18:39:24 +0300

    Initial commit (project structure)

---
+++ online-shop-again/src/main/java/com/example/onlineshopagain/OnlineShopAgainApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopAgainApplication.class, args);
+	}
+
+}
