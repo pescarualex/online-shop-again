cf286352185f6320e32ae655c23c7f898d81dbf5
pescarualex <pescarualexflorin@gmail.com>
2022-07-09 19:37:50 +0300

    Added User entity and data source config

---
+++ online-shop-again/src/main/java/com/example/onlineshopagain/domain/Cart.java
@@ -0,0 +1,4 @@
+package com.example.onlineshopagain.domain;
+
+public class Cart {
+}
---
+++ online-shop-again/src/main/java/com/example/onlineshopagain/domain/User.java
@@ -0,0 +1,56 @@
+package com.example.onlineshopagain.domain;
+
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+
+//    @OneToOne
+//    private Cart cart;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
